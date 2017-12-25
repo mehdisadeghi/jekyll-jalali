371ed3b79a3ee58751023ee62c9ac207ee9c04cc
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-25 01:09:39 +0100

    Apply a trick for better string parsing.

--- lib/jekyll-jalali.rb
+++ lib/jekyll-jalali.rb
@@ -1,15 +1,17 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require "date"
 require "jalalidate"
 
 module Jekyll
   module JDate
 
     def jdate(date, options={})
-      jalali = JalaliDate.new(date)
+      jalali = JalaliDate.new(Date.parse date.to_s)
       return jalali.strftime(options)
     end
+
   end
 end
 
