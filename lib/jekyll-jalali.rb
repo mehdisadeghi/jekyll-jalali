e9abb36045ed4979c5fa701cadb04719bd84b5e6
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-25 01:09:56 +0100

    Set a default format.

--- lib/jekyll-jalali.rb
+++ lib/jekyll-jalali.rb
@@ -7,7 +7,7 @@ require "jalalidate"
 module Jekyll
   module JDate
 
-    def jdate(date, options={})
+    def jdate(date, options="%d %b %Y")
       jalali = JalaliDate.new(Date.parse date.to_s)
       return jalali.strftime(options)
     end
